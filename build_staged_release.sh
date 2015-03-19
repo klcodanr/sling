1c9f22b08b1f7a0789cdd582c33f944ebcae1439
Dan Klco <dan.klco@sixdimensions.com>
2015-03-19 17:25:54 -0400

    Adding initial copy of the build staged release script

---
+++ build_staged_release.sh
@@ -0,0 +1,174 @@
+#!/bin/sh
+OPTIND=2
+
+function print_help {
+	bold=`tput bold`
+	normal=`tput sgr0`
+	echo "Usage: build_staged_release.sh <staging-number> [-d temp-directory] [-p port] [-hxk]"
+	echo "${bold}Parameters:${normal}"
+	echo "     -h/?    Display this message."
+	echo "     -d      (optional) set the download directory, default is /tmp/sling-build"
+	echo "     -k      (optional) kills the Sling process after the build and tests have completed"
+	echo "     -p      (optional) the port to start Sling on"
+	echo "     -x      (optional) skips the deployment and integration tests for this build, will not start Sling"
+	exit $STOP_CODE
+}
+
+# Initialize our variables
+STAGING=$1
+NO_DEPLOY=0
+DOWNLOAD="/tmp/sling-build"
+PORT="8080"
+KILL=0
+STOP_CODE=0
+
+while getopts "h?d:p:xk" opt; do
+	case "$opt" in
+	h|\?)
+		print_help
+		;;
+	d)  DOWNLOAD=$OPTARG
+		;;
+	x)  NO_DEPLOY=1
+		;;
+	k)  KILL=1
+		;;
+	p)  PORT=$OPTARG
+		;;
+	esac
+done
+
+if [ "$STAGING" -eq "-1" ]
+then
+	print_help
+	exit 1
+fi
+
+echo "Creating directory $DOWNLOAD..."
+mkdir -p ${DOWNLOAD} 2>/dev/null
+mkdir -p ${DOWNLOAD}/logs 2>/dev/null
+mkdir -p ${DOWNLOAD}/run 2>/dev/null
+mkdir -p ${DOWNLOAD}/build 2>/dev/null
+mkdir -p ${DOWNLOAD}/staging 2>/dev/null
+
+if [ ! -e "${DOWNLOAD}/${STAGING}" ]
+then
+	echo "################################################################################"
+	echo "						   DOWNLOAD STAGED REPOSITORY						   "
+	echo "################################################################################"
+	
+	mkdir -p ${DOWNLOAD}/staging/${STAGING}
+	if [ `wget --help | grep "no-check-certificate" | wc -l` -eq 1 ]
+	then
+		CHECK_SSL=--no-check-certificate
+	fi
+	
+	echo "Downloading repository http://repository.apache.org/content/repositories/orgapachesling-${STAGING}/org/apache/sling/..."
+	wget $CHECK_SSL \
+		-e "robots=off" --wait 1 -nv -r -np "--accept=pom" "--follow-tags=" \
+		-P "${DOWNLOAD}/staging/${STAGING}" -nH "--cut-dirs=3" --ignore-length \
+		"http://repository.apache.org/content/repositories/orgapachesling-${STAGING}/org/apache/sling/"
+else
+	echo "################################################################################"
+	echo "					   USING EXISTING STAGED REPOSITORY						 "
+	echo "################################################################################"
+	echo "${DOWNLOAD}/staging/${STAGING}"
+fi
+
+if [ "$NO_DEPLOY" -eq "0" ]
+then
+	echo "################################################################################"
+	echo "					   SETTING UP SLING INSTANCE								"
+	echo "################################################################################"
+	rm -r ${DOWNLOAD}/run 2> /dev/null
+	rm ${DOWNLOAD}/logs/sling-*.log 2> /dev/null
+	if [ ! -e "${DOWNLOAD}/build/sling" ]; then
+		echo "Downloading Sling SVN Repo to ${DOWNLOAD}/build/sling"
+		mkdir -p ${DOWNLOAD}/build/sling
+		svn co http://svn.apache.org/repos/asf/sling/trunk/ ${DOWNLOAD}/build/sling/ > /dev/null 2>&1
+	else 
+		svn up ${DOWNLOAD}/build/sling/ > /dev/null 2>&1
+	fi	
+	echo "Building Sling trunk..."
+	mvn clean install -f ${DOWNLOAD}/build/sling/pom.xml -DskipTests=true > ${DOWNLOAD}/logs/sling-build.log 2>&1
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		echo "mvn: BAD!! : Failed to build Sling trunk, see ${DOWNLOAD}/logs/sling-build.log"
+		exit 1
+	else
+		echo "mvn: GOOD : Successfully built Sling trunk"
+	fi
+	echo "Starting Sling instance at ${DOWNLOAD}/build/sling/launchpad/builder/target/*standalone.jar on port ${PORT}..."
+	java -jar ${DOWNLOAD}/build/sling/launchpad/builder/target/*standalone.jar -c ${DOWNLOAD}/run/sling -p $PORT > ${DOWNLOAD}/logs/sling-start.log 2>&1 &
+	PID=$!
+	echo "Sling started successfully, process: $PID"
+fi
+
+echo "################################################################################"
+echo "						  RUNNING MAVEN BUILD(s)								"
+echo "################################################################################"
+for i in `find "${DOWNLOAD}/staging/${STAGING}" -type f | grep '\.\(pom\)$'`
+do
+	sed -e 's/xmlns="http:\/\/maven\.apache\.org\/POM\/4\.0\.0"//g' $i > ${DOWNLOAD}/staging/${STAGING}/pom.xml
+	TAG=`xmllint --xpath '/project/scm/connection/text()' ${DOWNLOAD}/staging/${STAGING}/pom.xml`
+	VERSION=`xmllint --xpath '/project/version/text()' ${DOWNLOAD}/staging/${STAGING}/pom.xml`
+	TAG=${TAG/scm:svn:/} 
+	ARTIFACT_ID=`xmllint --xpath '/project/artifactId/text()' ${DOWNLOAD}/staging/${STAGING}/pom.xml`
+	echo "Running build for $TAG..."
+	mkdir -p ${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID
+	echo "Exporting tag $TAG to ${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID..."
+	svn export --force $TAG ${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID > ${DOWNLOAD}/logs/${STAGING}-$ARTIFACT_ID-build.log 2>&1
+	echo "Building tag ${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID..."
+	mvn clean install -f ${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID/pom.xml >> ${DOWNLOAD}/logs/${STAGING}-$ARTIFACT_ID-build.log 2>&1
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		echo "mvn: BAD!! : Failed to build $ARTIFACT_ID, see ${DOWNLOAD}/logs/${STAGING}-$ARTIFACT_ID-build.log"
+		exit 1
+	else 
+		echo "mvn: GOOD : Successfully built $ARTIFACT_ID"
+		if [ "$NO_DEPLOY" -eq "0" ]
+		then 
+			curl -s -u admin:admin -F "action=install" -F "_noredir_=_noredir_" -F "bundlefile=@${DOWNLOAD}/build/${STAGING}/$ARTIFACT_ID/target/$ARTIFACT_ID-$VERSION.jar" -F "bundlestart=start" http://localhost:${PORT}/system/console/bundles
+			sleep 30
+			RES=$(curl -s -u admin:admin http://localhost:${PORT}/system/console/bundles/$ARTIFACT_ID -F action=start)
+			if [[ $RES == *"32"* ]]
+			then
+				echo "bundle: GOOD : Successfully installed/started bundle $ARTIFACT_ID"
+			else
+				echo "bundle: BAD!! : Failed to install/start bundle $ARTIFACT_ID, response $RES"
+				STOP_CODE=1
+			fi
+		fi
+	fi
+done
+if [ "$NO_DEPLOY" -eq "0" ]; then 
+	echo "################################################################################"
+	echo "						  CHECK INTEGRATION TESTS							   "
+	echo "################################################################################"
+	mvn clean install  -Dhttp.port=${PORT} -Dtest.host=localhost -f ${DOWNLOAD}/build/sling/launchpad/integration-tests/pom.xml -Dtest=**/integrationtest/**/*Test.java > ${DOWNLOAD}/logs/${STAGING}-it.log 2>&1
+	rc=$?
+	if [[ $rc != 0 ]] ; then
+		echo "mvn: BAD!! : Failed to run integration tests, see ${DOWNLOAD}/logs/${STAGING}-it.log"
+		STOP_CODE=1
+	else
+		echo "mvn: GOOD : Successfully ran integration tests"
+	fi
+fi
+
+
+if [ "$KILL" -eq "1" ]
+then
+	echo "################################################################################"
+	echo "						  Cleaning Up										   "
+	echo "################################################################################"
+	if ps -p $PID > /dev/null 2>&1; then
+		kill $PID
+		echo "Stopped Sling process $PID..."
+	else
+		echo "Process ${PID} not running"
+	fi
+fi
+
+echo "################################################################################"
+
+exit $STOP_CODE
